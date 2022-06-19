48a08ec1cfa0171cd8038f8402adcfb700bc58a8
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2022-06-19 23:30:22 +0200

    Coverage.

--- envsrc/source_test.go
+++ envsrc/source_test.go
@@ -71,6 +71,25 @@ func Test_EnvSource(t *testing.T) {
 		assertVal(t, vals, path1, val1)
 		assertVal(t, vals, path2, "")
 	})
+	t.Run("handle ignore missing values", func(t *testing.T) {
+		env1 := gofakeit.Generate("TEST_ENV_1_{word}")
+		env2 := gofakeit.Generate("TEST_ENV_1_{word}")
+		path1 := gofakeit.Generate("test/path-1/{word}")
+		path2 := gofakeit.Generate("test/path-1/{word}")
+		val1 := gofakeit.SentenceSimple()
+		os.Setenv(env1, val1)
+		defer os.Unsetenv(env1)
+		source := New(
+			Set(path1).From(env1),
+			Set(path2).From(env2),
+		)
+		vals, err := source.ReadValues([]string{path1, path2})
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.Len(t, vals, 1)
+		assertVal(t, vals, path1, val1)
+	})
 	t.Run("no support of updated values", func(t *testing.T) {
 		env1 := gofakeit.Generate("TEST_ENV_1_{word}")
 		env2 := gofakeit.Generate("TEST_ENV_1_{word}")
